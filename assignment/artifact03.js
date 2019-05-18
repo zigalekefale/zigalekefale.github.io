1f4147d5ded9870689518f8adf4d1da23e02780d
Zigale <49889892+zigalekefale@users.noreply.github.com>
2019-05-18 12:34:25 -0700

    Update artifact03.js

--- assignment/artifact03.js
+++ assignment/artifact03.js
@@ -92,11 +92,7 @@ function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
 for (i=pHeight;i>= 0;i--){
 rLine +="<p>";
-// Fill in the left
-  
-for (x=1; x>=pHeight-i;x--){
-  rLine +="--";}
-  
+
 //Create each line on the Rhombus
 for(j=1;j<i;j++){
 
