70d006bf0d766cb61911b3b75a62d4d3b8d99bde
Zigale <49889892+zigalekefale@users.noreply.github.com>
2019-05-18 17:54:44 -0700

    Update module07.js

--- assignment/module07.js
+++ assignment/module07.js
@@ -8,11 +8,10 @@ function Hotel(name, rooms, booked) {
   };
 }
 
-
 // Create two hotel objects
 var quayHotel = new Hotel('Quay', 40, 25);
 var parkHotel = new Hotel('Park', 120, 77);
-
+var sunsetHotel = new Hotel('Sunset',86, 10);
 
 // Update the HTML for the page
 var details1 = quayHotel.name + ' rooms: ';
@@ -24,6 +23,11 @@ var details2 = parkHotel.name + ' rooms: ';
     details2 += parkHotel.checkAvailability();
 var elHotel2 = document.getElementById('hotel2');
 elHotel2.textContent = details2;
+
+var details3 = sunsetHotel.name + ' rooms: ';
+    details3 += sunsetHotel.checkAvailability();
+var elHotel3 = document.getElementById('hotel3');
+elHotel3.textContent = details3;
 
 /* 
 NOTE: textContent does not work in IE8 or earlier
