4cdad14936bb96674920b420595f9dfaf48447ed
Zigale <49889892+zigalekefale@users.noreply.github.com>
2019-05-23 15:03:05 -0700

    Create module08.js

---
+++ assignment/module08.js
@@ -0,0 +1,17 @@
+function addItem(){
+var newItem = document.getElementById('item').value;
+
+// Create a new element and store it in a variable.
+var newEl = document.createElement('li');
+
+// Create a text node and store it in a variable.
+var newText = document.createTextNode(newItem);
+
+// Attach the new text node to the new element.
+newEl.appendChild(newText);
+
+// Find the position where the new element should be added.
+var position = document.getElementsByTagName('ul')[0];
+
+// Insert the new element into its position.
+position.appendChild(newEl);}
