a785709eda9dc5b26896475855bd03c972bef256
Zigale <49889892+zigalekefale@users.noreply.github.com>
2019-05-27 18:59:31 -0700

    Update artifact05.js

--- assignment/images.js/artifact05.js
+++ assignment/images.js/artifact05.js
@@ -1,19 +1,19 @@
-function changeImage(){
-  var intervalId;
+var intervalId;
+
   function startSlideShow(){
-    intervalId = setInterval(changeImage, 500);
+    intervalId = setInterval(changeImage, 1000);
   }
   function stopSlideShow(){
-    clearInterval(intervalId)
+    clearInterval(intervalId);
   }
   function changeImage(){
     var imageSrc = document.getElementById("image").getAttribute("src");
     var currentImageNumeber = imageSrc.substing(imageSrc.lastIndexOf("/") + 1, imageSrc.lastIndexOf("/") + 2);
-    if (currentImageNumber == 8)
+    if (currentImageNumber == 5)
       {
         currentImageNumber = 0;
       }
   }
  
-  document.getElementById("image").setattribute("src", "/Images/" + (Number(currentImageNumber) + 1) + ".jpg");
+  document.getElementById("image").setAttribute("src", "/Images/" + (Number(currentImageNumber) + 1) + ".jpg");
 }
