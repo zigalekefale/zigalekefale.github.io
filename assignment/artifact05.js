ecd76498ad724aefbb8057d04d6ce72602d23d96
Zigale <49889892+zigalekefale@users.noreply.github.com>
2019-05-27 18:12:16 -0700

    Update artifact05.js

--- assignment/artifact05.js
+++ assignment/artifact05.js
@@ -1,22 +1,25 @@
-var i = 0; 			// Start Point
-var images = [];	// Images Array
-var time = 1000;	// Time Between Switch
-// Change Image
-function changeImg(){
-	document.slide.src = images[i];
-
-	// Check If Index Is Under Max
-	if(i < images.length - 1){
-	  // Add 1 to Index
-	  i++; 
-	} else { 
-		// Reset Back To O
-		i = 0;
-	}
-
-	// Run function every x seconds
-	setTimeOut("changeImg()", time);
+/*image lists
+var i = 0
+images[0] = 'https://i2.wp.com/www.desktopanimated.com/wp-content/uploads/2013/08/Green_Fields_1.jpg?resize=150%2C150';
+images[1] = 'https://www.99inspiration.com/wp-content/uploads/2017/03/Beautiful-Nature-Landscape-Photography-77-198x145.jpg';
+images[2] = 'http://www.fulldose.net/wp-content/themes/Fulldose/scripts/timthumb.php?src=http://www.fulldose.net/wp-content/uploads/2016/01/Autumn-at-Lake-Dock-Thousand-Islands-Canada.jpg&w=162&h=162'; 
+*/
+function changeImage(){
+  var intervalId;
+  function startSlideShow(){
+    intervalId = setInterval(changeImage, 500);
+  }
+  function stopSlideShow(){
+    clearInterval(intervalId)
+  }
+  function changeImage(){
+    var imageSrc = document.getElementById("image").getAttribute("src");
+    var currentImageNumeber = imageSrc.substing(imageSrc.lastIndexOf("/") + 1, imageSrc.lastIndexOf("/") + 2);
+    if (currentImageNumber == 8)
+      {
+        currentImageNumber = 0;
+      }
+  }
+ 
+  document.getElementById("image").setattribute("src", "/Images/" + (Number(currentImageNumber) + 1) + ".jpg");
 }
-
-// Run function when page loads
-window.onload=changeImg;
