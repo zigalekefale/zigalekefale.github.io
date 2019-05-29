f0f19ec3840c9104edfb6838eb4124cf349c1932
Zigale <49889892+zigalekefale@users.noreply.github.com>
2019-05-28 23:58:39 -0700

    Update artifact04.js

--- assignment/artifact04.js
+++ assignment/artifact04.js
@@ -1,4 +1,4 @@
-function ValidateFrom() {
+function ValidateForm(){
    var validUsername = false;
    var validUserpassword = false;
    var validFirstName = false;
@@ -15,11 +15,11 @@ function ValidateFrom() {
 
    /***********VALIDATES USERNAEM ******** */
    //Required. Maximum 12 characters.
-   if (myContact.username.value.lenght > 12 ||
+   if (myContact.username.value.length > 12 ||
      myContact.username.value===null || 
-     myContact.username.value==="") {
-          errorMessages += "<p>The username must be less than 20 characters and is required</P>";
-   else
+     myContact.username.value===""){
+          errorMessages += "<p>The username must be less than 20 characters and is required</P>";}
+   else {
        validUsername =true;}
 
    //console.log(validUsername);
@@ -27,28 +27,28 @@ function ValidateFrom() {
   /***********VALIDATES PASSWORD ******** */
       if (myContact.password.value==null || 
           myContact.password.value=== "" ||
-          myContact.password.value.lenght > 7){
-      errorMessages += "<p>The password must be less than 7 characters and it is required</P>";
-      else
+          myContact.password.value.length > 7){
+      errorMessages += "<p>The password must be less than 7 characters and it is required</P>";}
+      else {
          validUserpassword =true;}
 
       /***********VALIDATES FIRSTNAME ******** */
       if (myContact.firstname.value==null ||
           myContact.firstname.value=== "" || 
-          myContact.firstname.value.lenght >20 ||
-          !myContact.firstname.value.match(letters))
-      errorMessages += "<p>The firstname must be less than 20 characters and it is required. only letters and numbers are accepted</P>";
-      else
-      validFirstName =true;
+          myContact.firstname.value.length >20 ||
+          !myContact.firstname.value.match(letters)){
+      errorMessages += "<p>The firstname must be less than 20 characters and it is required. only letters and numbers are accepted</P>";}
+      else {
+      validFirstName =true;}
 
       /***********VALIDATES PHONE NUMBER ******** */
       if (myContact.Phone.value==null ||
           myContact.Phone.value=== "" ||
-          myContact.Phone.value.lenght >15 || 
-          !myContact.Phone.value.match(numbers))
-      errorMessages += "<p>The phone number must be less than 15 characters and it is required. only numbers are accepted</P>";
-      else
-      validPhone =true;
+          myContact.Phone.value.length >15 || 
+          !myContact.Phone.value.match(numbers)){
+      errorMessages += "<p>The phone number must be less than 15 characters and it is required. only numbers are accepted</P>";}
+      else {
+      validPhone =true;}
 
       document.getElementById("errorMessages").innerHTML = errorMessages;
       //make sure you return all the boolean variable that are checking each filed
