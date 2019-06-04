57515a97af8f6483ab480874012e5cae748a7c2e
Zigale <49889892+zigalekefale@users.noreply.github.com>
2019-06-03 22:09:53 -0700

    Update artifact04.js

--- assignment/artifact04.js
+++ assignment/artifact04.js
@@ -1,10 +1,16 @@
-
 function ValidateForm(){
-   var validUsername = false;
-   var validUserpassword = false;
-   var validFirstName = false;
-   var validPhone = false;
-   
+  var validFirstname = false;
+  var validLastname = false;
+  var validEmail = false;
+  var validUsername = false;
+  var validUserPassword = false;
+  var validPhone = false;
+  var validAddress = false;
+  var validCity = false;
+  var validCountry = false;
+  var validZipcode = false;
+  
+ 
   var letters = /^[A-Za-z]+$/;
   var numbers = /^[0-9]+$/;
    
@@ -24,6 +30,23 @@ function ValidateForm(){
        validUsername =true;
 
    //console.log(validUsername);
+  
+  /***********VALIDATES FIRSTNAME ******** */
+      if (myContact.firstname.value==null ||
+          myContact.firstname.value=== "" || 
+          myContact.firstname.value.length >20 ||
+          !myContact.firstname.value.match(letters))
+      errorMessages += "<p>The firstname must be less than 20 characters and it is required. only letters and numbers are accepted</P>";
+      else 
+      validFirstName =true;
+
+  /***************VALIDATES LASTNAME********/
+   if (document.getElementById("lastname").value.length > 0 &&
+    document.getElementById("lastname").value.length <= 50)
+    validLastname = true;
+  else
+    errorMessages += "<p> The lastname must be less than or equal to 50 characters";
+
      
   /***********VALIDATES PASSWORD ******** */
       if (myContact.password.value==null || 
@@ -33,15 +56,7 @@ function ValidateForm(){
       else 
          validUserPassword =true;
 
-      /***********VALIDATES FIRSTNAME ******** */
-      if (myContact.firstname.value==null ||
-          myContact.firstname.value=== "" || 
-          myContact.firstname.value.length >20 ||
-          !myContact.firstname.value.match(letters))
-      errorMessages += "<p>The firstname must be less than 20 characters and it is required. only letters and numbers are accepted</P>";
-      else 
-      validFirstName =true;
-
+     
       /***********VALIDATES PHONE NUMBER ******** */
       if (myContact.phone.value==null ||
           myContact.phone.value=== "" ||
@@ -50,8 +65,38 @@ function ValidateForm(){
       errorMessages += "<p>The phone number must be less than 15 characters and it is required. only numbers are accepted</P>";
       else 
       validPhone =true;
+  
+  /*****               ******/
+    if (myContact.address.value == null ||
+    myContact.address.value === "")
+    errorMessages += "<p> An Address is required</p>";
+    else
+    validAddress = true;
+  
+  /*************/
+  
+   if (myContact.city.value == null ||
+    myContact.city.value === "")
+    errorMessages += "<p> A City is required</p>";
+  else
+    validCity = true;
+  /**************************/
+  
+   if (myContact.countries.value == null ||
+    myContact.countries.value === "")
+    errorMessages += "<p> A Country is required</p>";
+  else
+    validCountry = true;
+  
+  /*****************************/
 
+  if (myContact.countries.value == 3)
+    if (myContact.zipcode.value.length === 5)
+      validZipcode = true
+    else
+      errorMessages += "<p>A Zip Code is required if the chosen country is USA.</p>";
+  
       document.getElementById("errorMessages").innerHTML = errorMessages;
       //make sure you return all the boolean variable that are checking each filed
-      return (validUsername && validUserpassword && validFirstName && validPhone) ;
+      return (validFirstname && validLastname && validEmail && validUsername && validUserpassword && validPhone && validAddress && validCity && validZipcode);
     }
