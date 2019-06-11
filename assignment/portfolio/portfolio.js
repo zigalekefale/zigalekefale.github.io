8b3f6980c5d6474568af27962c5fb92ae6389159
Zigale <49889892+zigalekefale@users.noreply.github.com>
2019-06-11 09:03:38 -0700

    Update portfolio.js

--- assignment/portfolio/portfolio.js
+++ assignment/portfolio/portfolio.js
@@ -1,3 +1,23 @@
 
-var d = new Date();
-document.getElementById("demo").innerHTML = d;
+var currentDate = new Date(),
+     day = currentDate.getDate(),
+     month = currentDate.getMonth() + 1,
+     year = currentDate.getFullYear();
+ //document.write(day + "/" + month + "/" + year)
+ var currentTime = new Date(),
+       hours = currentTime.getHours(),
+       minutes = currentTime.getMinutes();
+
+ 	if (minutes < 10) {
+ 	minutes = "0" + minutes;
+   }
+
+ 	//document.write(hours + ":" + minutes)
+
+
+
+var showdate = document.getElementById("demo").innerHTML;
+//showdate.style.color= "red";
+//showdate.textContent= day + "/" + month + "/" + year;
+ document.getElementById("demo").innerHTML= day + "/" + month + "/" + year + "  " + hours + ":" + minutes;
+
